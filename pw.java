c4ddc50de901852d5f12a4371f97f4ee73c78cc3
Dhirendra8923 <dhirendrapandey8923@gmail.com>
2023-01-24 23:25:00 +0530

    first commit

---
+++ pw.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter first number");
+	    int num_1 = sc.nextInt();
+	     System.out.println("Enter second number");
+	    int num_2 = sc.nextInt();
+	    int sum = num_1 + num_2;
+	    
+		System.out.println("Sum of two number is: "  + sum );
+	}
+}
